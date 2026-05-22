598c5c2086199fa3273ca5604f11d4c4ccf4a2af
tofuzzito <111707189+tofuzzito@users.noreply.github.com>
2026-05-22 19:51:22 -0300

    Revert "feat: implementar todas as subclasses da estrutura de Pagamento"
    This reverts commit 2747cd53a54a9fbcfee6b47d075995a198b295af.

--- classes/pagamento.py
+++ classes/pagamento.py
@@ -4,40 +4,17 @@ from datetime import date
 class Pagamento(ABC):
     def __init__(self, data: date, valorPago: float):
         self.data = data
-        self.valorPago = float(valorPago)
+        self.valorPago = valorPago
 
     @abstractmethod
-    def calcularRestante(self, valor_total: float) -> float:
-        """Método abstrato que deve ser implementado por todas as subclasses."""
+    def calcularRestante(self) -> float:
         pass
 
-
-class PagamentoPix(Pagamento):
-    def __init__(self, data: date, valorPago: float, cpfPagador: str):
-        # O super().__init__ chama o construtor da classe mãe (Pagamento)
-        super().__init__(data, valorPago)
-        self.cpfPagador = str(cpfPagador)
-
-    def calcularRestante(self, valor_total: float) -> float:
-        """Calcula o quanto falta pagar subtraindo o valor pago do valor total."""
-        restante = valor_total - self.valorPago
-        return max(0.0, restante)  # Garante que não retorne valores negativos
-
-
-# --- Mantendo o esqueleto das outras classes para o seu projeto ---
-
 class PagamentoDinheiro(Pagamento):
-    def calcularRestante(self, valor_total: float) -> float:
-        restante = valor_total - self.valorPago
-        return max(0.0, restante)
-
+    pass
 
 class PagamentoCartao(Pagamento):
-    def __init__(self, data: date, valorPago: float, numeroCartao: str, bandeira: str):
-        super().__init__(data, valorPago)
-        self.numeroCartao = str(numeroCartao)
-        self.bandeira = str(bandeira)
+    pass
 
-    def calcularRestante(self, valor_total: float) -> float:
-        restante = valor_total - self.valorPago
-        return max(0.0, restante)
+class PagamentoPix(Pagamento):
+    pass
