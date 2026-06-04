d708f8c66193add7c78d3d1d87b4c097ba8be453
Otto Nagel <ottonagel84@gmail.com>
2026-06-04 02:11:11 -0300

    Cria ProfissionalView

---
+++ views/ProfissionalView.py
@@ -0,0 +1,44 @@
+class ProfissionalView:
+
+    def mostra_menu(self):
+        print("\n=== PROFISSIONAIS ===")
+        print("1 - Incluir")
+        print("2 - Alterar")
+        print("3 - Excluir")
+        print("4 - Listar")
+        print("0 - Voltar")
+
+        return input("Escolha uma opção: ")
+
+    def pega_dados_profissional(self):
+
+        print("\n=== DADOS DO PROFISSIONAL ===")
+
+        nome = input("Nome: ")
+        celular = input("Celular: ")
+        cpf = input("CPF: ")
+        especialidade = input("Especialidade: ")
+        registro_profissional = input("Registro profissional: ")
+
+        return {
+            "nome": nome,
+            "celular": celular,
+            "cpf": cpf,
+            "especialidade": especialidade,
+            "registro_profissional": registro_profissional
+        }
+
+    def pega_cpf(self):
+        return input("CPF do profissional: ")
+
+    def mostra_profissional(self, profissional):
+
+        print(
+            f"Nome: {profissional.nome} | "
+            f"CPF: {profissional.cpf} | "
+            f"Especialidade: {profissional.especialidade} | "
+            f"Registro: {profissional.registro_profissional}"
+        )
+
+    def mostra_mensagem(self, mensagem):
+        print(mensagem)
